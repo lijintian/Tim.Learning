4f8b753a3d9377922c80a5ed49f9e2a59e8bcdec
jintian.li <jintian.li@chukou1.com>
2018-04-28 18:25:30 +0800

    Add:log4net.Appender.RollingFileAppender

--- Tim.Learning/Log4NetDemo/Program.cs
+++ Tim.Learning/Log4NetDemo/Program.cs
@@ -12,15 +12,19 @@ namespace Log4NetDemo
     {
         static void Main(string[] args)
         {
-            ILog log = log4net.LogManager.GetLogger("TestLogger.TestInherit");
-            
-            log.Error("错误", new Exception("发生了一个异常"));//错误
-            log.Fatal("严重错误", new Exception("发生了一个致命错误"));//严重错误
-            log.Info("信息"); //记录一般信息
-            log.Debug("调试信息");//记录调试信息
-            log.Warn("警告");//记录警告信息
+            for (var i = 0; i < 100; i++)
+            {
+                ILog log = log4net.LogManager.GetLogger("TestLogger.TestInherit");
 
-            Console.WriteLine("日志记录完毕。");
+                log.Error("错误", new Exception("发生了一个异常"));//错误
+                log.Fatal("严重错误", new Exception("发生了一个致命错误"));//严重错误
+                log.Info("信息"); //记录一般信息
+                log.Debug("调试信息");//记录调试信息
+                log.Warn("警告");//记录警告信息
+
+                Console.WriteLine("日志记录完毕。");
+            }
+            
 
             Console.Read();
         }
--- Tim.Learning/QuartzDemo/Program.cs
+++ Tim.Learning/QuartzDemo/Program.cs
@@ -49,11 +49,19 @@ namespace QuartzDemo
         {
             JobKey key = context.JobDetail.Key;
 
-            JobDataMap dataMap = context.JobDetail.JobDataMap;
+            //JobDataMap dataMap = context.JobDetail.JobDataMap;
+
+            JobDataMap dataMap = context.MergedJobDataMap;
 
             string jobSays = dataMap.GetString("jobSays");
 
             float myFloatValue = dataMap.GetFloat("myFloatValue");
+
+            IList<DateTimeOffset> state = (IList<DateTimeOffset>)dataMap["myStateData"];
+
+            state.Add(DateTimeOffset.UtcNow);
+
+            Console.Error.WriteLine("Instance " + key + " of DumbJob says: " + jobSays + ", and val is: " + myFloatValue);
 
             ILog log = log4net.LogManager.GetLogger("TestLogger.TestInherit");
  
