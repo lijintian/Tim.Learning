8948a75a71e8ca1f0b5519ec1616d266d1ba8997
jintian.li <jintian.li@chukou1.com>
2018-05-02 17:24:39 +0800

    add:尝试SmtpAppender，由于第三方的smtp服务器诸多限时，没有成功

--- Tim.Learning/Log4NetDemo/Program.cs
+++ Tim.Learning/Log4NetDemo/Program.cs
@@ -12,19 +12,16 @@ namespace Log4NetDemo
     {
         static void Main(string[] args)
         {
-            for (var i = 0; i < 100; i++)
+            ILog log = log4net.LogManager.GetLogger("SmtpLogger");
+
+            for (var i = 0; i < 1; i++)
             {
-                ILog log = log4net.LogManager.GetLogger("TestLogger.TestInherit");
-
-                log.Error("错误", new Exception("发生了一个异常"));//错误
-                log.Fatal("严重错误", new Exception("发生了一个致命错误"));//严重错误
-                log.Info("信息"); //记录一般信息
-                log.Debug("调试信息");//记录调试信息
-                log.Warn("警告");//记录警告信息
-
-                Console.WriteLine("日志记录完毕。");
+                log.Error("错误" + i.ToString(), new Exception("发生了一个异常"));//错误
+                log.Fatal("严重错误" + i.ToString(), new Exception("发生了一个致命错误"));//严重错误
+                log.Info("信息" + i.ToString()); //记录一般信息
+                log.Debug("调试信息" + i.ToString());//记录调试信息
+                log.Warn("警告" + i.ToString());//记录警告信息
             }
-            
 
             Console.Read();
         }
