8ee3ef40b6610ab09b283385438fc330c422b6ce
jintian.li <jintian.li@chukou1.com>
2019-06-06 15:53:59 +0800

    Add：Pipeline模式

---
+++ Tim.Learning/OOD/Pipeline/AbstractValve.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOD.Pipeline
+{
+    //阀门抽象类
+    public abstract class AbstractValve
+    {
+        private AbstractValve _nextValve;
+        public AbstractValve GetNext()
+        {
+            return _nextValve;
+        }
+        public void SetNext(AbstractValve v)
+        {
+            _nextValve = v;
+        }
+        public abstract void Invoke(string s);
+
+    }
+}
